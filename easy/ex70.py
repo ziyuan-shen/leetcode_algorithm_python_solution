d98daa87cab340d57345c99c4fb38b0d62c2d0c8
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-10-11 13:01:09 -0400

    update ex70 with dynamic programming

--- easy/ex70.py
+++ easy/ex70.py
@@ -2,16 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        max_2 = n // 2
-        num = 0
-        def factorial(n):
-            if n==0:
-                return 1
-            product = 1
-            for i in range(1, n+1):
-                product *= i
-            return product
-        for num_2 in range(1, max_2+1):
-            position = n - num_2
-            num += (factorial(position) / factorial(position - num_2) / factorial(num_2))
-        return int(num + 1)
+        if n == 2:
+            return 2
+        else:
+            prev2_step = 1
+            prev_step = 2
+            for i in range(3, n + 1):
+                current_step = prev2_step + prev_step
+                prev2_step = prev_step
+                prev_step = current_step
+            return current_step
