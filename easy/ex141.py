70161146de6d83778e86ee2dd91980b3248f2308
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-10-27 22:28:37 -0400

    add ex141 constant memory solution

--- easy/ex141.py
+++ easy/ex141.py
@@ -8,10 +8,12 @@ class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         if head == None:
             return False
-        current_node = head
-        l = []
-        while current_node != None:
-            l.append(current_node)
-            current_node = current_node.next
-            if current_node in l:
+        slow_pointer = head
+        fast_pointer = head.next
+        while True:
+            if slow_pointer == fast_pointer:
                 return True
+            if slow_pointer.next == None or fast_pointer.next==None or fast_pointer.next.next == None:
+                return False
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
