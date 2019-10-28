4b6f6ae2b281699c4439ebd5e4b3cb88648ed267
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-10-27 23:22:30 -0400

    add ex350

---
+++ easy/ex350.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = []
+        for num in nums1:
+            if num in nums2:
+                intersection.append(num)
+                nums2.remove(num)
+        return intersection
+        
