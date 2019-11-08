688622877d1ee83f11f58f9d3c18ea6e0efb73e7
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-11-07 21:13:18 -0500

    add ex1192 bridge algorithm

--- hard/ex1192.py
+++ hard/ex1192.py
@@ -5,33 +5,53 @@ class Solution(object):
         :type connections: List[List[int]]
         :rtype: List[List[int]]
         """
-        def isConnected(connections):
-            visited_dic = {i: 0 for i in range(n)}
-            node_stack = [0]
-            while len(node_stack) > 0:
-                current_node = node_stack[-1]
-                add_node_flag = False
-                for path in connections:
-                    if path[0] == current_node or path[1] == current_node:
-                        if path[0] == current_node:
-                            connected_node = path[1]
-                        else:
-                            connected_node = path[0]
-                        if visited_dic[connected_node]==0:
-                            visited_dic[connected_node] = 1
-                            current_node = connected_node
-                            node_stack.append(current_node)
-                            add_node_flag = True
-                            break
-                if add_node_flag:
-                    continue
-                else:
-                    node_stack.pop()
-            return sum(visited_dic.values()) == n
-        critical_connections = []
+        node_stack = [0]
+        visited_nodes = [0]
+        id_dic = {0: 0}
+        current_id = 0
+        directed_connections = []
+        while len(node_stack)>0:
+            current_node = node_stack[-1]
+            find_node_flag = False
+            for path in connections:
+                if path[0]==current_node or path[1]==current_node:
+                    if path[0]==current_node:
+                        connected_node = path[1]
+                    else:
+                        connected_node = path[0]
+                    if connected_node not in visited_nodes:
+                        visited_nodes.append(connected_node)
+                        node_stack.append(connected_node)
+                        current_id += 1
+                        id_dic[connected_node] = current_id
+                        directed_connections.append([current_node, connected_node])
+                        find_node_flag = True
+                        break
+            if not find_node_flag:
+                node_stack.pop()
         for path in connections:
-            connections_copy = connections[:]
-            connections_copy.remove(path)
-            if not isConnected(connections_copy):
-                critical_connections.append(path)
-        return critical_connections
+            if not (path in directed_connections or path[::-1] in directed_connections):
+                if id_dic[path[0]] < id_dic[path[1]]:
+                    path = path[::-1]
+                directed_connections.append(path)
+        low_link_dic = {i: id_dic[i] for i in range(n)}
+        for i in range(n):
+            connected_nodes = [i]
+            node_stack = [i]
+            while len(node_stack) > 0:
+                add_node_flag = False
+                current_node = node_stack[-1]
+                for path in directed_connections:
+                    if path[0]==current_node and path[1] not in connected_nodes:
+                        connected_nodes.append(path[1])
+                        node_stack.append(path[1])
+                        add_node_flag = True
+                        break
+                if not add_node_flag:
+                    node_stack.pop()
+            low_link_dic[i] = min([id_dic[j] for j in connected_nodes])
+        bridges = []
+        for path in directed_connections:
+            if id_dic[path[0]] < low_link_dic[path[1]]:
+                bridges.append(path)
+        return bridges
