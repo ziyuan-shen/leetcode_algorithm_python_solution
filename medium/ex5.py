6d0a8fc3d3e0dd44483511d797aaf3a003eec68a
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-11-09 21:36:58 -0500

    add ex5 dp solution

--- medium/ex5.py
+++ medium/ex5.py
@@ -1,18 +1,19 @@
 class Solution:
-    def isPalindrome(self, s):
-        idx = 0
-        while idx < len(s) // 2:
-            if s[idx] != s[-1-idx]:
-                return False
-            idx += 1
-        return True
-        
     def longestPalindrome(self, s: str) -> str:
-        max_length = len(s)
-        while max_length>0:
-            for i in range(len(s)-max_length+1):
-                substring = s[i:i+max_length]
-                if self.isPalindrome(substring):
-                    return substring
-            max_length -= 1
-        return ''
+        length = len(s)
+        if length==0:
+            return ''
+        DP = [[False]*i for i in range(length, 0, -1)]
+        max_substring = s[0]
+        for i in range(length):
+            DP[i][0] = True
+        for i in range(length-1):
+            DP[i][1] = (s[i]==s[i+1])
+            if DP[i][1]:
+                max_substring = s[i:i+2]
+        for i in range(2, length):
+            for j in range(0, length-i):
+                DP[j][i] = (s[j] == s[i+j]) and DP[j+1][i-2]
+                if DP[j][i]:
+                    max_substring = s[j:j+i+1]
+        return max_substring
