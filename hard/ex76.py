ceb386dde4b56d226e05309228f1e84e777349c7
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-11-13 19:51:58 -0500

    add ex76 correct solution

--- hard/ex76.py
+++ hard/ex76.py
@@ -1,35 +1,49 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        dic = {char: -1 for char in t}
+        num_dic = {}
+        for char in t:
+            if char not in num_dic:
+                num_dic[char] = 1
+            else:
+                num_dic[char] += 1
+        count_dic = {char: 0 for char in t}
+        idx_dic = {char: [] for char in t}
         idx = 0
         window = False
         left = None
         right = None
         while idx < len(s):
             while (not window) and idx < len(s):
-                while left == None:
-                    if s[idx] in t:
+                while idx < len(s) and left == None:
+                    if s[idx] in num_dic:
                         left = idx
-                        dic[s[idx]] = idx
+                        count_dic[s[idx]] = 1
+                        idx_dic[s[idx]].append(idx)
                     idx += 1
-                if idx<len(s) and s[idx] in t:
-                    dic[s[idx]] = max(dic[s[idx]], idx)
+                if idx<len(s) and s[idx] in num_dic:
+                    if num_dic[s[idx]] == count_dic[s[idx]]:
+                        idx_dic[s[idx]].pop(0)
+                    else:
+                        count_dic[s[idx]] += 1
+                    idx_dic[s[idx]].append(idx)
                 window = True
-                for m in dic.values():
-                    if m == -1:
+                for char in num_dic:
+                    if num_dic[char] != count_dic[char]:
                         window = False
                 idx += 1
             if not window:
                 return ''
             if right == None:
-                right = max(dic.values())
+                right = max(list(map(max, idx_dic.values())))
+                left = min(list(map(min, idx_dic.values())))
                 min_window = s[left:right+1]
             if idx >= len(s):
                 return min_window
-            if s[idx] in dic:
-                dic[s[idx]] = idx
+            if s[idx] in num_dic:
+                idx_dic[s[idx]].pop(0)
+                idx_dic[s[idx]].append(idx)
                 right = idx
-                left = min(dic.values())
+                left = min(list(map(min, idx_dic.values())))
                 if (right - left + 1) < len(min_window):
                     min_window = s[left:right+1]
             idx += 1
