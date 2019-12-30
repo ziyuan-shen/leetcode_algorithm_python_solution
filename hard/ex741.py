d52cf770aa2f35a52d4efd59a4ff58b4250c9a66
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-12-29 22:32:15 -0500

    add ex741

--- hard/ex741.py
+++ hard/ex741.py
@@ -1,51 +1,27 @@
-class Solution:
-    def get_cherry(self, grid):
-        '''
-        returns:
-        int: number of cherries collected
-        list[list]: updated grid after collection
-        bool: if there is a valid path
-        '''
-        nrow = len(grid)
-        ncol = len(grid[0])
-        current_node = grid[0][0]
-        if current_node == -1:
-            return 0, grid, False
-        current_cherry = current_node
-        if nrow > 1 and ncol > 1:
-            right_cherry, right_grid, right_valid = self.get_cherry([row[1:] for row in grid])
-            down_cherry, down_grid, down_valid = self.get_cherry(grid[1:])
-            if right_cherry > down_cherry:
-                new_grid = [[grid[i][0]] + right_grid[i] for i in range(nrow)]
-                new_grid[0][0] = 0
-                return current_cherry + right_cherry, new_grid, True
-            else:
-                if down_valid:
-                    new_grid = [grid[0]] + down_grid
-                    new_grid[0][0] = 0
-                    return current_cherry + down_cherry, new_grid, True
-                else:
-                    return 0, grid, False
-        else:
-            if nrow == 1 and ncol == 1:
-                return current_cherry, [[0]], True
-            if nrow == 1:
-                right_cherry, right_grid, right_valid = self.get_cherry([grid[0][1:]])
-                if right_valid:
-                    return current_cherry + right_cherry, [[0] + right_grid[0]], True
-                else:
-                    return 0, grid, False
-            if ncol == 1:
-                down_cherry, down_grid, down_valid = self.get_cherry(grid[1:])
-                if down_valid:
-                    return current_cherry + down_cherry, [[0]] + down_grid, True
-                else:
-                    return 0, grid, False
-                
+class Solution: 
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        cherry, new_grid, valid = self.get_cherry(grid)
-        if valid:
-            cherry += self.get_cherry([row[::-1] for row in new_grid[::-1]])[0]
-            return cherry
-        else:
-            return 0
+        N = len(grid)
+        dp = [[[float('-inf') for i in range(N)] for i in range(N)] for i in range(N)]
+        dp[N-1][N-1][N-1] = grid[-1][-1]
+        for t in range(2 * N - 3, -1 , -1):
+            for r1 in range(min(t, N - 1), max(0, t - N + 1) - 1, -1):
+                for r2 in range(min(t, N - 1), max(0, t - N + 1) - 1, -1):
+                    c1 = t - r1
+                    c2 = t - r2
+                    if grid[r1][c1] == -1 or grid[r2][c2] == -1:
+                        continue
+                    else:
+                        values = []
+                        if r1 + 1 < N:
+                            values.append(dp[r1+1][c1][r2])
+                            if r2 + 1 < N:
+                                values.append(dp[r1+1][c1][r2+1])
+                        if c1 + 1 < N:
+                            values.append(dp[r1][c1+1][r2])
+                            if r2 + 1 < N:
+                                values.append(dp[r1][c1+1][r2+1])
+                        dp[r1][c1][r2] = grid[r1][c1] + grid[r2][c2] + max(values)
+                        if r1 == r2 and c1 == c2 and grid[r1][c1] == 1:
+                            dp[r1][c1][r2] -= 1
+        return max(0, dp[0][0][0])
+                
