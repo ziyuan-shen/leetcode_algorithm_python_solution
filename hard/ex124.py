c7ec0941d0cce23c08ddaacdae88e985f0b07867
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-12-31 04:17:52 -0500

    add ex124 LRV solution

--- hard/ex124.py
+++ hard/ex124.py
@@ -6,24 +6,42 @@
 #         self.right = None
 
 class Solution:
-    def maxNodePath(self, root):
+    def copyTree(self, root):
         if root == None:
             return None
-        if root.left == None and root.right == None:
-            return root.val
-        if root.right == None:
-            return max(self.maxNodePath(root.left) + root.val, root.val)
-        if root.left == None:
-            return max(self.maxNodePath(root.right) + root.val, root.val)
-        return max(self.maxNodePath(root.left) + root.val, self.maxNodePath(root.right) + root.val, root.val)
-        
-    def maxPathSum(self, root: TreeNode) -> int:
-        if root.left == None and root.right == None:
-            return root.val
-        if root.right == None:
-            return max(self.maxPathSum(root.left), self.maxNodePath(root))
-        if root.left == None:
-            return max(self.maxPathSum(root.right), self.maxNodePath(root))
-        left = self.maxNodePath(root.left)
-        right = self.maxNodePath(root.right)
-        return max(left + right + root.val, left + root.val, right + root.val, root.val, self.maxPathSum(root.left), self.maxPathSum(root.right))
+        new = TreeNode(root.val)
+        new.left = self.copyTree(root.left)
+        new.right = self.copyTree(root.right)
+        return new
+    
+    def maxPathSum(self, root):
+        root_copy = self.copyTree(root)
+        def nodeLRV(root):
+            if root != None:
+                nodeLRV(root.left)
+                nodeLRV(root.right)
+                if root.right == None and root.left == None:
+                    return None
+                elif root.right == None:
+                    root.val += max(root.left.val, 0)
+                elif root.left == None:
+                    root.val += max(root.right.val, 0)
+                else:
+                    root.val += max(root.left.val, root.right.val, 0)
+        nodeLRV(root_copy)
+        def pathLRV(root, node_root):
+            if root != None:
+                pathLRV(root.left, node_root.left)
+                pathLRV(root.right, node_root.right)
+                if root.right == None and root.left == None:
+                    return None
+                elif root.right == None:
+                    root.val = max(root.left.val, node_root.val)
+                elif root.left == None:
+                    root.val = max(root.right.val, node_root.val)
+                else:
+                    left = node_root.left.val
+                    right = node_root.right.val
+                    root.val = max(left+right+root.val, left+root.val, right+root.val, root.val, root.left.val, root.right.val)
+        pathLRV(root, root_copy)
+        return root.val
