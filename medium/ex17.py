6f4525d83a1fcc10e0a8b50be7a704e42ab1d47b
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-01 01:05:35 -0500

    add ex17

---
+++ medium/ex17.py
@@ -0,0 +1,12 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        dic = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
+        ans = [""]
+        for i in range(len(digits)):
+            new = []
+            for letter in dic[digits[i]]:
+                new += [s + letter for s in ans]
+            ans = new
+        return ans
