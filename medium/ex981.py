8015950a0e4fdb2ab0fda15923901c106e1a0b38
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-02 17:06:00 -0500

    add ex981 bisect solution

--- medium/ex981.py
+++ medium/ex981.py
@@ -1,22 +1,10 @@
+import bisect
 class TimeMap:
     def __init__(self):
         """
         Initialize your data structure here.
         """
         self.data = {}
-    
-    def binarySearch(self, l, target):
-        low = 0
-        high = len(l) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if l[mid] <= target:
-                if mid == len(l) - 1 or l[mid+1] > target:
-                    return mid
-                low = mid + 1
-            else:
-                high = mid - 1
-        return -1
         
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key in self.data:
@@ -28,7 +16,7 @@ class TimeMap:
         if key not in self.data:
             return ""
         else:
-            idx = self.binarySearch([x[0] for x in self.data[key]], timestamp)
+            idx = bisect.bisect(self.data[key], [timestamp, chr(126)]) - 1
             if idx != -1:
                 return self.data[key][idx][1]
             return ""
