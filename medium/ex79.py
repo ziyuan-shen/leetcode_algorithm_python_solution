7544855fe8fe51fb02b1d5502e000f8672412098
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-02 15:39:42 -0500

    add ex79

--- medium/ex79.py
+++ medium/ex79.py
@@ -1,32 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        if board == []:
+            return False
         nrow = len(board)
         ncol = len(board[0])
         for i in range(nrow):
             for j in range(ncol):
-                if board[i][j] != word[0]:
-                    continue
-                visited = [[i, j]]
-                idx = 1
-                fail = {k: [] for k in range(len(word))}
-                while idx < len(word):
-                    start = visited[idx-1]
-                    neighbors = [[start[0]-1, start[1]], [start[0]+1, start[1]], [start[0], start[1]-1], [start[0], start[1]+1]]
-                    flag = False
-                    for n in neighbors:
-                        if n[0] >= 0 and n[0] < nrow and n[1] >= 0 and n[1] < ncol:
-                            if board[n[0]][n[1]] == word[idx] and n not in visited and n not in fail[idx]:
-                                visited.append(n)
-                                idx += 1
-                                flag = True
-                                break
-                    if not flag:
-                        fail[idx-1].append(start)
-                        visited.pop(-1)
-                        idx -= 1
-                        if idx == 0:
-                            break
-                if idx != 0:
+                if self.dfs(board, i, j, 0, word):
                     return True
         return False
-                        
+    
+    def dfs(self, board, i, j, idx, word):
+        if idx == len(word):
+            return True
+        else:
+            nrow = len(board)
+            ncol = len(board[0])
+            if i >= 0 and i < nrow and j >= 0 and j < ncol and board[i][j] == word[idx]:
+                original = board[i][j]
+                board[i][j] = ""
+                if self.dfs(board, i-1, j, idx+1, word) or self.dfs(board, i+1, j, idx+1, word) or self.dfs(board, i, j-1, idx+1, word) or self.dfs(board, i, j+1, idx+1, word):
+                    return True
+                board[i][j] = original
+                return False
+            else:
+                return False
