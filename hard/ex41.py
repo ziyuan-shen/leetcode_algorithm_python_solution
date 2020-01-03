1514103a084d908976ff077ecadcfdb946e8959e
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-02 19:05:37 -0500

    add ex41

---
+++ hard/ex41.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        if 1 not in nums:
+            return 1
+        for i in range(len(nums)):
+            if nums[i] <= 0 or nums[i] > len(nums):
+                nums[i] = 1
+        for i in range(len(nums)):
+            if nums[abs(nums[i])-1] > 0:
+                nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return i + 1
+        return len(nums) + 1
