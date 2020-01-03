45f7f85781750ddce6e60b4f617b50a5d301faff
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-03 01:13:48 -0500

    update ex127

--- medium/ex127.py
+++ medium/ex127.py
@@ -1,35 +1,27 @@
+from collections import defaultdict
+from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
         if beginWord in wordList:
             wordList.remove(beginWord)
-        wordList.insert(0, beginWord)
         wordlen = len(beginWord)
-        edges = []
-        for i in range(len(wordList)-1):
-            for j in range(i+1, len(wordList)):
-                if sum([wordList[i][k] == wordList[j][k] for k in range(wordlen)]) == wordlen - 1:
-                    edges.append([i, j])
-        visited = [[0]]
-        visitedseq = [0]
-        flag = True
-        while len(visitedseq) < len(wordList) and flag:
-            flag = False
-            prevs = visited[-1]
-            visited.append([])
-            for prev in prevs:
-                for edge in edges:
-                    if edge[0] == prev and edge[1] not in visitedseq:
-                        if wordList[edge[1]] == endWord:
-                            return len(visited)
-                        visited[-1].append(edge[1])
-                        visitedseq.append(edge[1])
-                        flag = True
-                    if edge[1] == prev and edge[0] not in visitedseq:
-                        if wordList[edge[0]] == endWord:
-                            return len(visited)
-                        visited[-1].append(edge[0])
-                        visitedseq.append(edge[0])
-                        flag = True
+        predic = defaultdict(list)
+        for word in wordList:
+            for i in range(wordlen):
+                predic[word[:i] + '*' + word[i+1:]].append(word)
+        q = deque([(beginWord, 1)])
+        visited = {}
+        while q:
+            word, level = q.popleft()
+            nextwords = []
+            for i in range(wordlen):
+                nextwords.extend(predic[word[:i] + '*' + word[i+1:]])
+            for nextword in nextwords:
+                if nextword == endWord:
+                    return level + 1
+                if nextword not in visited:
+                    visited[nextword] = True
+                    q.append((nextword, level+1))
         return 0
