f4874944c66dba051080205c38466cf233bfc89f
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-11 01:49:33 -0500

    add ex239 deque solution

--- hard/ex239.py
+++ hard/ex239.py
@@ -1,8 +1,21 @@
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         if not nums:
             return []
-        ans = []
-        for i in range(len(nums) - k + 1):
-            ans.append(max(nums[i:i+k]))
+        if k == 1:
+            return nums
+        q = deque([])
+        for i in range(k):
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+            q.append(i)
+        ans = [nums[q[0]]]
+        for i in range(k, len(nums)):
+            if q and q[0] == i - k:
+                q.popleft()
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+            q.append(i)
+            ans.append(nums[q[0]])
         return ans
