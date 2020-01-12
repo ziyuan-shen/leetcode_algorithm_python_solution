271122cfa5b37d9a7c337990fa3a7dfd6057ae28
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-11 19:02:53 -0500

    add ex44 dp solution

--- hard/ex44.py
+++ hard/ex44.py
@@ -1,13 +1,13 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not bool(s)
-        if p[0] == "?":
-            return s != "" and self.isMatch(s[1:], p[1:])
-        elif p[0] == "*":
-            if not s:
-                return self.isMatch(s, p[1:])
-            else:
-                return self.isMatch(s, p[1:]) or self.isMatch(s[1:], p)
-        else:
-            return s != "" and s[0] == p[0] and self.isMatch(s[1:], p[1:])
+        dp = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
+        dp[-1] = [p[j:] == (len(p) - j) * "*" for j in range(len(p) + 1)]
+        for i in range(len(s) - 1, -1 , -1):
+            for j in range(len(p) - 1, -1, -1):
+                if p[j] == "?":
+                    dp[i][j] = dp[i+1][j+1]
+                elif p[j] == "*":
+                    dp[i][j] = dp[i][j+1] or dp[i+1][j]
+                else:
+                    dp[i][j] = s[i] == p[j] and dp[i+1][j+1]
+        return dp[0][0]
