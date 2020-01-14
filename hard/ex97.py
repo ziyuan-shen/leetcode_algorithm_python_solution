dce591685784f2b15ab86e7817deb7cf5eff035a
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-14 13:17:49 -0500

    add ex97 dp solution

--- hard/ex97.py
+++ hard/ex97.py
@@ -1,14 +1,25 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if s1 == "" and s2 == "" and s3 == "":
-            return True
-        if len(s1) + len(s2) != len(s3):
+        n1 = len(s1)
+        n2 = len(s2)
+        n3 = len(s3)
+        if n1 + n2 != n3:
             return False
-        if len(s1) > 0 and len(s2) > 0 and s1[0] == s2[0]:
-            return s3[0] == s1[0] and (self.isInterleave(s1[1:], s2, s3[1:]) or self.isInterleave(s1, s2[1:], s3[1:]))
-        elif len(s1) > 0 and s1[0] == s3[0]:
-            return self.isInterleave(s1[1:], s2, s3[1:])
-        elif len(s2) > 0 and s2[0] == s3[0]:
-            return self.isInterleave(s1, s2[1:], s3[1:])
-        else:
-            return False
+        dp = [[[False for _ in range(n3+1)] for _ in range(n2+1)] for _ in range(n1+1)]
+        dp[-1][-1][-1] = True
+        for i in range(n1):
+            for j in range(n3):
+                dp[i][-1][j] = s1[i:] == s3[j:]
+        for i in range(n2):
+            for j in range(n3):
+                dp[-1][i][j] = s2[i:] == s3[j:]
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                for k in range(n3-1, -1, -1):
+                    if s1[i] == s2[j]:
+                        dp[i][j][k] = s3[k] == s1[i] and (dp[i+1][j][k+1] or dp[i][j+1][k+1])
+                    elif s1[i] == s3[k]:
+                        dp[i][j][k] = dp[i+1][j][k+1]
+                    elif s2[j] == s3[k]:
+                        dp[i][j][k] = dp[i][j+1][k+1]
+        return dp[0][0][0]
