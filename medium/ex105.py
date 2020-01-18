50edb1e4066b6f6ec77205a1b43f236199532af2
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-18 14:21:21 -0500

    add ex105

---
+++ medium/ex105.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+        left_inorder = inorder[:idx]
+        right_inorder = inorder[idx+1:]
+        left_preorder = preorder[1:1+len(left_inorder)]
+        right_preorder = preorder[1+len(left_inorder):]
+        root.left = self.buildTree(left_preorder, left_inorder)
+        root.right = self.buildTree(right_preorder, right_inorder)
+        return root
