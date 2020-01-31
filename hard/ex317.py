db8273e11c5c1a93c9b969a07e2b74373ceab68e
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-30 22:01:16 -0500

    update ex317

--- hard/ex317.py
+++ hard/ex317.py
@@ -17,30 +17,29 @@ class Solution:
                         buildings.add(num)
                     else:
                         lands.add(num)
-                        for n in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
-                            r = n[0]
-                            c = n[1]
-                            if r >= 0 and r < nrow and c >= 0 and c < ncol and grid[r][c] != 2:
-                                neighbordic[num].add(r * ncol + c)
-        nodes = buildings.union(lands)
-        mindistance = float("inf")
-        for land in lands:
-            visited = {node: False for node in nodes}
-            q = deque([(0, land)])
-            visited[land] = True
-            distance = 0
-            visited_buildings_num = 0
+                    for n in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                        r = n[0]
+                        c = n[1]
+                        if r >= 0 and r < nrow and c >= 0 and c < ncol and grid[r][c] == 0:
+                            neighbordic[num].add(r * ncol + c)
+        reached = defaultdict(lambda: defaultdict(int))
+        for building in buildings:
+            visited = {land: False for land in lands}
+            q = deque([(0, building)])
             while q:
                 level, node = q.popleft()
                 for neighbor in neighbordic[node]:
                     if not visited[neighbor]:
                         visited[neighbor] = True
                         q.append((level + 1, neighbor))
-                        if neighbor in buildings:
-                            distance += level + 1
-                            visited_buildings_num += 1
-                if visited_buildings_num == len(buildings) or distance > mindistance:
-                    break
-            if visited_buildings_num == len(buildings):
-                mindistance = min(mindistance, distance)
-        return -1 if mindistance == float("inf") else mindistance
+                        reached[building][neighbor] = level + 1
+        if len(reached) == len(buildings):
+            commonlands = set(list(reached.values())[0].keys())
+            for dic in reached.values():
+                commonlands = commonlands.intersection(set(dic.keys()))
+        else:
+            commonlands = set()
+        if len(commonlands) == 0:
+            return -1
+        else:
+            return min([sum([reached[b][land] for b in buildings]) for land in commonlands])
