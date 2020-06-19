a26ce571375a786e7639f7ea56113d87bf8c9b4e
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-06-19 19:09:09 -0400

    add ex622

---
+++ medium/ex622.py
@@ -0,0 +1,69 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.k = k
+        self.head = 0
+        self.tail = 0
+        self.queue = [0 for _ in range(k+1)]
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.isFull():
+            return False
+        else:
+            self.tail = (self.tail + 1) % (self.k + 1)
+            self.queue[self.tail] = value
+            return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        else:
+            self.head = (self.head + 1) % (self.k + 1)
+            return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[(self.head + 1) % (self.k + 1)]
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.tail]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.head == self.tail
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return ((self.tail + 1) % (self.k + 1)) == self.head
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
