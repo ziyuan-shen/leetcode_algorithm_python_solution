314553ddee5b579360a6bccf5956d7734b7ab817
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-06-20 19:21:28 -0400

    add ex351

--- medium/ex351.py
+++ medium/ex351.py
@@ -1,22 +1,28 @@
+from collections import defaultdict
 class Solution:
-    def numberOfPatterns(self, m: int, n: int) -> int:
-        l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        patterndict = {1: {(idx+1,): l[:idx] + l[idx+1:] for idx in range(9)}}
-        bridgedict = {(1, 3): 2, (4, 6): 5, (7, 9): 8, (1, 7): 4, (2, 8): 5, (3, 9): 6, (1, 9): 5, (3, 7): 5}
-        for i in range(2, 10):
-            patterndict[i] = {}
-            for pre in patterndict[i-1]:
-                for idx in range(len(patterndict[i-1][pre])):
-                    num = patterndict[i-1][pre][idx]
-                    if (pre[-1], num) in bridgedict:
-                        if bridgedict[(pre[-1], num)] in pre:
-                            patterndict[i][pre+(num,)] = patterndict[i-1][pre][:idx] + patterndict[i-1][pre][idx+1:]
-                    elif (num, pre[-1]) in bridgedict:
-                        if bridgedict[(num, pre[-1])] in pre:
-                            patterndict[i][pre+(num,)] = patterndict[i-1][pre][:idx] + patterndict[i-1][pre][idx+1:]
+    def move(self, current):
+        if len(current) < 9:
+            for num in range(1, 10):
+                if num not in current:
+                    if (current[-1], num) in self.bridgedict:
+                        if self.bridgedict[(current[-1], num)] in current:
+                            self.patterns[len(current) + 1] += 1
+                            self.move(current + (num,))
+                    elif (num, current[-1]) in self.bridgedict:
+                        if self.bridgedict[(num, current[-1])] in current:
+                            self.patterns[len(current) + 1] += 1
+                            self.move(current + (num,))
                     else:
-                        patterndict[i][pre+(num,)] = patterndict[i-1][pre][:idx] + patterndict[i-1][pre][idx+1:]
+                        self.patterns[len(current) + 1] += 1
+                        self.move(current + (num,))
+            
+    def numberOfPatterns(self, m: int, n: int) -> int:
+        self.patterns = defaultdict(int)
+        self.patterns[1] = 9
+        self.bridgedict = {(1, 3): 2, (4, 6): 5, (7, 9): 8, (1, 7): 4, (2, 8): 5, (3, 9): 6, (1, 9): 5, (3, 7): 5}
+        for i in range(1, 10):
+            self.move((i,))
         ans = 0
         for i in range(m, n+1):
-            ans += len(patterndict[i])
+            ans += self.patterns[i]
         return ans
