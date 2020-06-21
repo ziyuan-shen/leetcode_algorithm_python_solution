b1fe84455fd3d938888a42796a721f9caa500d00
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-06-21 01:15:56 -0400

    add ex73-constant space solution

--- medium/ex73.py
+++ medium/ex73.py
@@ -1,27 +1,21 @@
 class Solution:
-    def setRowCol(self, matrix, row, col):
-        self.zero_nodes.add((row, col))
-        for i in range(len(matrix[0])):
-            if matrix[row][i] == 0 and (row, i) not in self.zero_nodes:
-                self.setRowCol(matrix, row, i)
-            matrix[row][i] = 0
-            self.zero_nodes.add((row, i))
-        for i in range(len(matrix)):
-            if matrix[i][col] == 0 and (i, col) not in self.zero_nodes:
-                self.setRowCol(matrix, i, col)
-            matrix[i][col] = 0
-            self.zero_nodes.add((i, col))
             
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        self.zero_nodes = set()
         length = len(matrix)
         width = len(matrix[0])
         for i in range(length):
             for j in range(width):
-                if (i, j) not in self.zero_nodes:
-                    if matrix[i][j] == 0:
-                        self.setRowCol(matrix, i, j)
-                        break
+                if matrix[i][j] == 0:
+                    for row in range(length):
+                        if matrix[row][j] != 0:
+                            matrix[row][j] = float("inf")
+                    for col in range(width):
+                        if matrix[i][col] != 0:
+                            matrix[i][col] = float("inf")
+        for i in range(length):
+            for j in range(width):
+                if matrix[i][j] == float("inf"):
+                    matrix[i][j] = 0
