65725ad84b94eb9a843dcb876d70621628f87714
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-06-28 22:34:17 -0400

    add ex678-dp solution

--- medium/ex678.py
+++ medium/ex678.py
@@ -1,22 +1,22 @@
-class Solution:
-    def isValid(self, q, s):
-        for idx, ch in enumerate(s):
-            if ch == ")":
-                if q:
-                    q.pop()
-                else:
-                    return False
-            elif ch == "(":
-                q.append("(")
-            else:
-                if q:
-                    if self.isValid(q[1:], s[idx+1:]):
-                        return True
-                if self.isValid(q + ["("], s[idx+1:]):
-                    return True
-                if self.isValid(q.copy(), s[idx+1:]):
-                    return True
-        return not q
-            
+class Solution:     
     def checkValidString(self, s: str) -> bool:
-        return self.isValid([], s)
+        if not s:
+            return True
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            if s[i] == "*":
+                dp[i][i] = True
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                if s[i] == "*":
+                    if dp[i+1][i+length-1]:
+                        dp[i][i+length-1] = True
+                        continue
+                if s[i] != ")":
+                    for j in range(i+1, i+length):
+                        if s[j] != "(":
+                            if (j-1<i+1 or dp[i+1][j-1]) and (i+length-1<j+1 or dp[j+1][i+length-1]):
+                                dp[i][i+length-1] = True
+                                break
+        return dp[0][n-1]
