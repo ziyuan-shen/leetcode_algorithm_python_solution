cc88b456039baf16eadfb084063e27001d9a1ab3
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-07-03 17:41:31 -0400

    add ex112

---
+++ easy/ex112.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == sum
+        elif not root.left:
+            return self.hasPathSum(root.right, sum - root.val)
+        elif not root.right:
+            return self.hasPathSum(root.left, sum - root.val)
+        else:
+            return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
