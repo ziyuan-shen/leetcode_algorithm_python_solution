6015693bd4de7301e2a7710537db469d6ca1229a
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-10-11 02:02:31 -0700

    add ex87

--- hard/ex87.py
+++ hard/ex87.py
@@ -1,24 +1,21 @@
-from collections import deque
 class Solution:
+    mem = {}
     def isScramble(self, s1: str, s2: str) -> bool:
-        q = deque([[s1]])
-        while q:
-            strings = q.popleft()
-            if "".join(strings) == s2:
+        if len(s1) == 1:
+            return s1 == s2
+        for i in range(1, len(s1)):
+            if (s1[:i], s2[:i]) not in self.mem:
+                self.mem[(s1[:i], s2[:i])] = self.isScramble(s1[:i], s2[:i])
+            a = self.mem[(s1[:i], s2[:i])]
+            if (s1[i:], s2[i:]) not in self.mem:
+                self.mem[(s1[i:], s2[i:])] = self.isScramble(s1[i:], s2[i:])
+            b = self.mem[(s1[i:], s2[i:])]
+            if (s1[:i], s2[-i:]) not in self.mem:
+               self.mem[(s1[:i], s2[-i:])] = self.isScramble(s1[:i], s2[-i:])
+            c = self.mem[(s1[:i], s2[-i:])]
+            if (s1[i:], s2[:-i]) not in self.mem:
+                self.mem[(s1[i:], s2[:-i])] = self.isScramble(s1[i:], s2[:-i])
+            d = self.mem[(s1[i:], s2[:-i])]
+            if (a and b) or (c and d):
                 return True
-            if len(strings) == len(s1):
-                continue
-            prevlen = 0
-            for i in range(len(strings)):
-                if len(strings[i]) == 1:
-                    prevlen += 1
-                else:
-                    for j in range(len(strings[i]) - 1):
-                        left = strings[i][:j+1]
-                        right = strings[i][j+1:]
-                        if not ((len(left) == 1 and left != s2[prevlen]) or (len(right) == 1 and right != s2[prevlen + len(left)])):
-                            q.append(strings[:i] + [left, right] + strings[i+1:])
-                        if not ((len(left) == 1 and left != s2[prevlen + len(right)]) or (len(right) == 1 and right != s2[prevlen])):
-                            q.append(strings[:i] + [right, left] + strings[i+1:])
-                    prevlen += len(strings[i])
         return False
