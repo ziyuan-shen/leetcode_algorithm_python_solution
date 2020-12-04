ccd927537928e623fdb22007e24dc1390f8c17df
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-12-04 02:38:04 -0800

    add ex219

---
+++ easy/ex219.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = dict()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                if i - seen[nums[i]] <= k:
+                    return True
+                else:
+                    seen[nums[i]] = i
+            else:
+                seen[nums[i]] = i
+        return False
