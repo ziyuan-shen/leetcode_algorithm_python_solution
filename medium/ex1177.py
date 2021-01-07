1aadfe6f4016036aebb39d4408a3e169d0f8a237
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-01-06 22:22:07 -0800

    update ex1177

--- medium/ex1177.py
+++ medium/ex1177.py
@@ -1,32 +1,18 @@
 from collections import Counter
 class Solution:
     def canMakePaliQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
-        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
-        for i in range(len(s)):
-            dp[i][i] = 0
-        for i in range(len(s) - 1):
-            if s[i] != s[i+1]:
-                dp[i][i+1] = 1
-        for i in range(len(s) - 2):
-            d = Counter(s[i:i+3])
-            oddnum = 0
-            for value in d.values():
-                if value % 2 == 1:
-                    oddnum += 1
-            dp[i][i+2] = (oddnum - 1) // 2
-            for j in range(i+3, len(s)):
-                if s[j] in d:
-                    d[s[j]] += 1
-                    if d[s[j]] % 2 == 1:
-                        oddnum += 1
-                    else:
-                        oddnum -= 1
-                else:
-                    oddnum += 1
-                    d[s[j]] = 1
-                dp[i][j] = (oddnum - ((j-i+1) % 2)) // 2
+        oddletters = [None for _ in range(len(s) + 1)]
+        oddletters[0] = set()
+        for i in range(1, len(s) + 1):
+            oddletters[i] = oddletters[i-1].copy()
+            if s[i-1] in oddletters[i-1]:
+                oddletters[i].remove(s[i-1])
+            else:
+                oddletters[i].add(s[i-1])
         ans = [False for _ in range(len(queries))]
         for idx, q in enumerate(queries):
-            if dp[q[0]][q[1]] <= q[2]:
+            s1 = oddletters[q[0]]
+            s2 = oddletters[q[1] + 1]
+            if (len(s1) + len(s2) - 2 * len(s1.intersection(s2)) - ((q[1] - q[0] + 1) % 2)) // 2 <= q[2]:
                 ans[idx] = True
         return ans
