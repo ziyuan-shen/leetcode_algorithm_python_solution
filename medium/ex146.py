a87be88dfc0870e16a4bafadd6e0bf8e932b8115
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-01-12 22:37:42 -0800

    update ex146

--- medium/ex146.py
+++ medium/ex146.py
@@ -1,45 +1,27 @@
-class LRUCache(object):
+from collections import OrderedDict
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.key_order = []
-        
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key in self.cache:
-            self.key_order.remove(key)
-            self.key_order = [key] + self.key_order
-            return self.cache[key]
-        else:
-            return -1
+        self.data = OrderedDict()
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        if key in self.cache:
-            self.cache[key] = value
-            self.key_order.remove(key)
-            self.key_order = [key] + self.key_order
+
+    def get(self, key: int) -> int:
+        if key in self.data:
+            self.data.move_to_end(key)
+            return self.data[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+            self.data.pop(key)
+            self.data[key] = value
         else:
-            if len(self.cache)<self.capacity:
-                self.cache[key] = value
-                self.key_order = [key]+self.key_order
+            if len(self.data) == self.capacity:
+                self.data.popitem(last=False)
+                self.data[key] = value
             else:
-                self.cache.pop(self.key_order[-1])
-                self.cache[key] = value
-                self.key_order = [key] + self.key_order[:-1]
-        
-
+                self.data[key] = value
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
