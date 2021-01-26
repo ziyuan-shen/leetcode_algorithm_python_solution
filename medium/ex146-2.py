ae9b869ef182a1d6d2555bef0b570599d91d63c9
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-01-25 16:51:50 -0800

    add ex146-2

---
+++ medium/ex146-2.py
@@ -0,0 +1,30 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.data = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.data:
+            self.data.move_to_end(key)
+            return self.data[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+            self.data[key] = value
+            self.data.move_to_end(key)
+        else:
+            if len(self.data) < self.capacity:
+                self.data[key] = value
+            else:
+                self.data.popitem(last=False)
+                self.data[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
