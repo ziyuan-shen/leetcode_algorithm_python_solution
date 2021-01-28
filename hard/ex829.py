248662118265e1087877cb0a1b3fe6e21adbebc8
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-01-28 01:35:54 -0800

    update ex829

--- hard/ex829.py
+++ hard/ex829.py
@@ -1,19 +1,10 @@
 class Solution:
-    def binaryCheck(self, N, x, low, high):
-        while low <= high:
-            mid = (low + high) // 2
-            target = (2 * x + mid - 1) * mid / 2
-            if target == N:
-                return True
-            if target < N:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return False
-        
     def consecutiveNumbersSum(self, N: int) -> int:
         ans = 0
-        for x in range(1, N+1):
-            if self.binaryCheck(N, x, 1, N - x + 1):
-                ans += 1
+        i = 1
+        while i * (i - 1) < 2 * N:
+            if 2 * N / i == 2 * N // i:
+                if (2 * N // i + 1 - i) % 2 == 0:
+                    ans += 1
+            i += 1
         return ans
